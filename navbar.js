af52ec32ff0541b5bea9e3172e50ab8859d085af
Randhir200 <mail.randhirkr@gmail.com>
2022-05-05 00:24:17 +0530

    now hamburger in well function

--- navbar.js
+++ navbar.js
@@ -1,5 +1,6 @@
 var menu_btn = document.querySelector('.hamburger');
-
+var mobile_nav = document.querySelector('.mobile-nav');
 menu_btn.addEventListener('click', function () {
   menu_btn.classList.toggle('is-active');
+  mobile_nav.classList.toggle('is-active');
 });
