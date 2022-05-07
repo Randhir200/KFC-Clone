ae36c86c854b192d24d81a9003a3766c979bfe9f
Randhir200 <mail.randhirkr@gmail.com>
2022-05-07 22:38:56 +0530

    commited

--- slider.js
+++ slider.js
@@ -30,3 +30,35 @@ slider.addEventListener('mousemove', function (event) {
   const walk = (x - startX)*3;
   slider.scrollLeft = scrollLeft - walk;
 });
+
+// variable
+const slider1 = document.querySelector('.quick-media-scroller');
+let isDown1 = false;
+let startX1;
+let scrollLeft1;
+
+// events
+slider1.addEventListener('mousedown', function (event) {
+  isDown1 = true;
+  slider1.classList.add('active');
+  startX1 = event.pageX - slider1.offsetLeft;
+  scrollLeft1 = slider1.scrollLeft;
+});
+
+slider1.addEventListener('mouseleave', function () {
+  isDown1 = false;
+  slider1.classList.remove('active');
+});
+
+slider1.addEventListener('mouseup', function () {
+  isDown1 = false;
+  slider1.classList.remove('active');
+});
+
+slider1.addEventListener('mousemove', function (event) {
+  if (!isDown1) return;
+  event.preventDefault();
+  const x = event.pageX - slider1.offsetLeft;
+  const walk1 = (x - startX1)*3;
+  slider1.scrollLeft = scrollLeft1 - walk1;
+});
