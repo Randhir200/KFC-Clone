d0cf3a5bc6c7f4031dbd1a3fb50dedf163e91c65
Randhir200 <mail.randhirkr@gmail.com>
2022-05-09 12:31:16 +0530

    mobile hamburger menu issue solved

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,15 @@
+//mobile-toggle-menu
+var menu_btn = document.querySelector('.hamburger');
+var mobile_nav = document.querySelector('.mobile-nav');
+menu_btn.addEventListener('click', function () {
+  menu_btn.classList.toggle('is-active');
+  mobile_nav.classList.toggle('is-active');
+});
+
+
 // variable
 var data = JSON.parse(localStorage.getItem('signinData'));
 var usertag = document.getElementById('usertag');
 var fullname = data.firstname[0] + data.lastname[0];
 usertag.innerText = fullname;
+
